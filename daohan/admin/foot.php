30827f1e5f714be42eaab55ce15999d52a0898ee
Allen-China <honglinhua@outlook.com>
2020-08-16 15:07:10 +0800

    daohan
    daohan

---
+++ daohan/admin/foot.php
@@ -0,0 +1,1 @@
+<div id="footer">Copyright &copy; <span style="color:#FF5200">发布</span><span style="color:#0065FF">平台</span>：七途导航</div>
---
+++ daohan/admin/inc.php
@@ -0,0 +1,5 @@
+<title>七途导航管理后台</title>
+<meta name="keywords" content="七途导航" />
+<meta name="description" content="七途导航" />
+<link href="./images/woaik.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="../img/logo.png">
